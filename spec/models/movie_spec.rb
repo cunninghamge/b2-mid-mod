dccfd4c692bab5d39d6990486e72b0bf2fc9655f
Grayson Cunningham <gecun289@gmail.com>
2021-01-04 09:30:43 -0700

    create model tests for relationships

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Movie do
+  describe 'relationships' do
+    it {should belong_to :studio}
+    it {should have_many :roles}
+    it {should have_many(:actors).through(:roles)}
+  end
+end
