65dbc0aad245538a07e33d2f6134d323e78d32a5
Grayson Cunningham <gecun289@gmail.com>
2021-01-04 11:52:50 -0700

    add flash message if actor does not exist

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,7 +1,12 @@
 class RolesController < ApplicationController
   def create
     actor = Actor.find_by_name(params[:role][:actor_name])
-    Role.create!(actor: actor, movie_id: params[:movie_id])
+    role = Role.new(actor: actor, movie_id: params[:movie_id])
+    if role.invalid?
+      flash[:notice] = "Unable to add unknown actor"
+    else
+      role.save
+    end
     redirect_to movie_path(params[:movie_id])
   end
 end
