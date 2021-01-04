57744f6e06e3c953556c2d00b1864923eeb51b8b
Grayson Cunningham <gecun289@gmail.com>
2021-01-04 10:03:33 -0700

    add tests for movie show page

---
+++ spec/features/movies/show_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "Movie Show" do
+  before :each do
+    @studio_1 = Studio.create!(name: "Universal Pictures", location: "Universal City, CA")
+    @movie_1 = @studio_1.movies.create!(title: "Cats", creation_year: 2019, genre: "musical")
+    @actor_1 = Actor.create!(name: "Taylor Swift", age: 31)
+    @actor_2 = Actor.create!(name: "Judi Dench",age: 86)
+    @actor_3 = Actor.create!(name: "Jason Derulo",age: 32)
+
+    visit movie_path(@movie_1)
+  end
+
+  it 'displays the attributes of the movie' do
+    expect(page).to have_content(@movie_1.title)
+    expect(page).to have_content(@movie_1.creation_year)
+    expect(page).to have_content(@movie_1.genre)
+  end
+
+  it 'lists all of the actors from youngest to oldest' do
+    expect(@actor_1.name).to appear_before(@actor_3.name)
+    expect(@actor_3.name).to appear_before(@actor_2.name)
+  end
+
+  it 'shows the average age of all of the actors' do
+    avg = Actor.average(:age).round(1)
+    expect(page).to have_content("Average Age: #{avg} years")
+  end
+end
